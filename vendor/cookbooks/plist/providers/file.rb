03db81f0abfa6b06e3144925e92488b3d3a20393
Roy Liu <carsomyr@gmail.com>
2017-05-08 15:26:56 -0400

    Update to `0.9.4`

--- vendor/cookbooks/plist/providers/file.rb
+++ vendor/cookbooks/plist/providers/file.rb
@@ -14,7 +14,6 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-require "chef/shell_out"
 require "diff/lcs"
 require "diff/lcs/hunk"
 require "etc"
